67bcb7982c37d89e34f93ec2a81922ea5993a9fa
predrag56 <predrag.vukovic@lineone.net>
2019-05-01 22:42:01 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 
 var express = require('express');
 var app = express();
-
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server); 
+
+var mongodb = require("mongodb");
+//var ObjectID = mongodb.ObjectID;
+
 //-------
 var add=(function(){var counter=0; return function(){return counter +=1;}})();
 //-------
-//--
-//----------- Manage command line arguments
-//var mongo = require('mongodb').MongoClient;
-///var myArgs = require('optimist').argv;
-//var mongoHost, mongoDBName;
-//--
+
 
 app.use('/css',express.static(__dirname + '/css'));
 app.use('/js',express.static(__dirname + '/js'));
@@ -27,30 +25,6 @@ server.lastPlayderID = 0;
 server.listen(process.env.PORT || 8081,function(){
     console.log('Listening on '+server.address().port);
 });
-//===============
-/*
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
-
-  // Initialize the app.
-  var serverx = app.listen(process.env.PORT || 8080, function () {
-    var port = serverx.address().port;
-    console.log("App now running on port", port);
-  });
-});
-*/
-//===============
 
 io.on('connection',function(socket){
 
