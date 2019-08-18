1a064b74591e83fabc5a855456349e1535805abe
predrag56 <predrag.vukovic@lineone.net>
2019-08-18 13:44:06 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server); 
 
+const testAddon = require('./build/Release/testaddon.node');
+
+console.log('addon', testAddon);
+console.log('hello ', testAddon.hello());
+console.log('add ', testAddon.add(5, 10));
+
+const prevInstance = new testAddon.ClassExample(4.3);
+console.log('Initial value : ', prevInstance.getValue());
+console.log('After adding 3.3 : ', prevInstance.add(3.3));
+
+const newFromExisting = new testAddon.ClassExample(prevInstance);
+
+console.log('Testing class initial value for derived instance');
+console.log(newFromExisting.getValue());
+
+
+
+
 
 var mongo = require('mongodb').MongoClient;
 //-------
@@ -19,6 +37,8 @@ app.use('/assets',express.static(__dirname + '/assets'));
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
 });
+
+module.exports = testAddon;
 
 server.lastPlayderID = 0;
 
