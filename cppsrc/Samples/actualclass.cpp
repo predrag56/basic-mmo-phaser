bc99f677f6062db5fcf9e6f8033293f6ef76e439
predrag56 <predrag.vukovic@lineone.net>
2019-08-22 17:12:43 +0100

    Add files via upload

--- cppsrc/Samples/actualclass.cpp
+++ cppsrc/Samples/actualclass.cpp
@@ -11,6 +11,18 @@ double ActualClass::getValue()
 
 double ActualClass::add(double toAdd)
 {
+  double k,kk;
+  kk=0;
   this->value_ += toAdd;
+  k=this->value_;
+  
+  this->value_=change( k,  kk);
   return this->value_;
+}
+
+double ActualClass::change(double k,double kk)
+{
+//k *= 11.0;
+	kk=k*111;
+return kk;
 }
--- cppsrc/Samples/actualclass.h
+++ cppsrc/Samples/actualclass.h
@@ -3,6 +3,7 @@ class ActualClass {
   ActualClass(double value);
   double getValue();
   double add(double toAdd);
+  double change(double k, double kk);
  private:
   double value_;
 };
